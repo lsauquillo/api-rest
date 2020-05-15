04bbf1079c0f9e06c951c5c92724817d25bd3afb
Luis <lsauquillo@gmail.com>
2020-05-15 10:56:35 +0200

    configuracion aparte

--- index.js
+++ index.js
@@ -4,22 +4,18 @@
 //Se pueden  presentar todos los productos, un productoId, agregar nuevo producto, 
 //actualizar un productoId, o eliminar un productoId de la coleccion.
 
-
 const mongoose = require('mongoose')
-mongoose.set('useFindAndModify', false);
-
+const config = require('./config')
 const app = require('./app')
 
-const port = process.env.PORT || 3001;
-
-mongoose.connect('mongodb://localhost:27017/shop', {useNewUrlParser: true, useUnifiedTopology: true }, (err, res)=>{
+mongoose.connect(config.db, {useNewUrlParser: true, useUnifiedTopology: true }, (err, res)=>{
   if(err) {
     return console.log(`Error al conectar a la base de datos: ${err}`)
   }
   console.log('Conexion a la base de datos establecida...')
 
-  app.listen(port, ()=>{
-    console.log(`API corriendo en http://localhost:${port}`)
+  app.listen(config.port, ()=>{
+    console.log(`API corriendo en http://localhost:${config.port}`)
   })
 
 })
